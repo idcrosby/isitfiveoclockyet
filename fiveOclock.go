a60bbe194f02acd835cead72169c840d06050d1d
Ian Crosby <icrosby@mashery.com>
2014-03-03 20:13:02 -0800

    Initial hacky fix for time zones

--- fiveOclock.go
+++ fiveOclock.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"net/http/httputil"
 	"net/http"
 	"time"
+	"bytes"
 	"text/template"
+	"strconv"
 )
 
 func main() {
@@ -12,16 +16,83 @@ func main() {
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("Request: %s", requestAsString(r))
 
-	var mainTemplate, _  = template.ParseFiles("drink.html")
-	var message string
-	if time.Now().Hour() > 16 {
-		message = "Yup"
+	if (r.Method == "GET") {
+		var mainTemplate, err  = template.ParseFiles("fiveOclock.html")
+		if err != nil {
+			w.WriteHeader(404)
+			return
+		}
+		mainTemplate.Execute(w, nil)
 	} else {
-		message = "Nope"
+		r.ParseForm()
+		var zone = r.FormValue("zone")
+		//fmt.Printf("Zone: %s", zone)
+
+		// Get Hour at GMT
+		// TODO assuming EST zone
+		var hour = time.Now().Hour() + 5
+		var minute = time.Now().Minute()
+
+		// TODO catch errors
+		var hourDiff, _ = strconv.Atoi(zone[4:6])
+		var minDiff, _ = strconv.Atoi(zone[7:9])
+
+		if (zone[:3] == "GMT") {
+			if (zone[3:4] == "-") {
+				if (minDiff > minute) {
+					hourDiff++
+				}
+				hour = hour - hourDiff % 24
+			} else if (zone[3:4] == "+") {
+				if (minDiff + minute > 60) {
+					hourDiff++
+				}
+				hour = hour + hourDiff % 24
+			} else {
+				//error
+			}
+		} else {
+			// Switch on all time zones?
+		}
+
+		//fmt.Printf("Adjusted local hour: %d", hour)
+		// fmt.Printf("PostForm: %s", r.Form)
+		var mainTemplate, err  = template.ParseFiles("fiveOclockWithZone.html")
+		if err != nil {
+			w.WriteHeader(404)
+			return
+		}
+		var message string
+		var offset = 17 - hour
+		if offset <= 0 {
+			message = "Yup"
+		} else {
+			message = "It's five o'clock somewhere"
+		}
+		var data = Data{Message: message}
+		// var name, offset = time.Now().Zone()
+		// fmt.Printf("Zone is %s with offset of %d", name, offset)
+		mainTemplate.Execute(w, data)
 	}
-	var data = Data{Message: message}
-	mainTemplate.Execute(w, data)
+}
+
+func getTimeZone() {
+	// time.Now().Zone
+}
+
+// Create a string which contains all important request data
+func requestAsString(request *http.Request) []byte {
+	var buffer bytes.Buffer
+	buffer.WriteString("\n")
+	buffer.WriteString("Current Time: ")
+	buffer.WriteString(time.Now().String())
+	buffer.WriteString("\n")
+	requestBytes, _ := httputil.DumpRequest(request, true)
+	buffer.Write(requestBytes)
+
+	return buffer.Bytes()
 }
 
 type Data struct {
